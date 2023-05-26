1cf0208019a658ded07b2407cda290f51d0c2aa0
leesihoo0919 <117376316+leesihoo0919@users.noreply.github.com>
2023-05-26 22:10:40 +0900

    Update GameMode3.h

--- GameMode3.h
+++ GameMode3.h
@@ -1,6 +1,14 @@
 #pragma once
 
 #include "SDL.h"
+#include <random>
+
+// 게임 상태
+enum class GameState {
+	Start, // 시작 상태
+	Playing, // 게임 플레이 중 상태
+	GameOver // 게임 오버 상태
+};
 
 class Mode3 : public PhaseInterface
 {
@@ -12,6 +20,58 @@ public:
 	virtual void Render();
 
 private:
+	//Tutorial
+	SDL_Rect g_tt_source_rect;
+	SDL_Rect g_tt_destination_rect;
+	SDL_Texture* g_tt_texture;
+
+	//stone
+	SDL_Rect g_stone_source_rect;
+	SDL_Rect g_stone_destination_rect;
+	SDL_Texture* stoneTextures[4];
+
+	//캐릭터
+	SDL_Rect g_ch_source_rect;
+	SDL_Rect g_ch_destination_rect;
+	SDL_Texture* chTextures[2];
+
+	//BackGround
+	SDL_Rect g_bg_source_rect;
+	SDL_Rect g_bg_destination_rect;
+	SDL_Texture* g_bg_texture;
+
+	//wall
+	SDL_Rect g_w_source_rect;
+	SDL_Rect g_w_destination_rect;
+	SDL_Texture* g_w_texture;
+
+	//잡은 돌
+	SDL_Rect g_caught_source_rect;
+	SDL_Rect g_caught_destination_rect;
+	SDL_Texture* g_caught_texture;
+
+	// Start Button
+	SDL_Texture* texture_;
+	SDL_Rect source_rectangle_;
+	SDL_Rect destination_rectangle_;
+
+	// Bakc Button
+	SDL_Texture* texture_bb;
+	SDL_Rect source_rectangle_bb;
+	SDL_Rect destination_rectangle_bb;
+
+	// Ready
+	SDL_Texture* texture_ready;
+	SDL_Rect source_rectangle_ready;
+	SDL_Rect destination_rectangle_ready;
+
+	bool tutorial;
+	bool ready;
+	bool game_start;
+	bool game_over;
+	
+
+
 
 };
 
